912b08b927ec62be1ca36096f3bf1bbb3ff06fdf
Kentaro Abe <kentaro@beah.jp>
2024-04-23 00:14:44 +0900

    add: Provider Run Settings

---
+++ controllers/provider.go
@@ -0,0 +1,31 @@
+package controllers
+
+import cloudprovider "k8s.io/cloud-provider"
+
+type Provider struct {
+	cloudprovider.Interface
+}
+
+func (p *Provider) LoadBalancer() (cloudprovider.LoadBalancer, bool) {
+	return &ServiceController{}, true
+}
+
+func (p *Provider) Instances() (cloudprovider.Instances, bool) {
+	return nil, false
+}
+
+func (p *Provider) InstancesV2() (cloudprovider.InstancesV2, bool) {
+	return nil, false
+}
+
+func (p *Provider) Zones() (cloudprovider.Zones, bool) {
+	return nil, false
+}
+
+func (p *Provider) Routes() (cloudprovider.Routes, bool) {
+	return nil, false
+}
+
+func (p *Provider) HasClusterID() bool {
+	return true
+}
--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"github.com/bear-san/haproxy-ccm/controllers"
+	"io"
+	"k8s.io/apimachinery/pkg/util/wait"
+	cloudprovider "k8s.io/cloud-provider"
+	"k8s.io/cloud-provider/app"
+	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
+	"k8s.io/cloud-provider/names"
+	"k8s.io/cloud-provider/options"
+	"k8s.io/component-base/cli"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/klog/v2"
+	"os"
+)
+
 func main() {
-	// This is a placeholder for the main function.
+	ccmOptions, err := options.NewCloudControllerManagerOptions()
+	if err != nil {
+		panic(err)
+	}
+
+	controllerInitializers := app.DefaultInitFuncConstructors
+	controllerAliases := names.CCMControllerAliases()
+
+	fss := cliflag.NamedFlagSets{}
+
+	command := app.NewCloudControllerManagerCommand(ccmOptions, cloudInitializer, controllerInitializers, controllerAliases, fss, wait.NeverStop)
+	code := cli.Run(command)
+	os.Exit(code)
+}
+
+func cloudInitializer(config *cloudcontrollerconfig.CompletedConfig) cloudprovider.Interface {
+	cloud, err := cloudprovider.InitCloudProvider("haproxy", config.ComponentConfig.KubeCloudShared.CloudProvider.CloudConfigFile)
+	if err != nil {
+		klog.Fatalf("Cloud provider could not be initialized: %v", err)
+	}
+	if cloud == nil {
+		klog.Fatalf("Cloud provider is nil")
+	}
+
+	if !cloud.HasClusterID() {
+		if config.ComponentConfig.KubeCloudShared.AllowUntaggedCloud {
+			klog.Warning("detected a cluster without a ClusterID.  A ClusterID will be required in the future.  Please tag your cluster to avoid any future issues")
+		} else {
+			klog.Fatalf("no ClusterID found.  A ClusterID is required for the cloud provider to function properly.  This check can be bypassed by setting the allow-untagged-cloud option")
+		}
+	}
+
+	return cloud
+}
+
+func init() {
+	cloudprovider.RegisterCloudProvider("haproxy", func(config io.Reader) (cloudprovider.Interface, error) {
+		return &controllers.Provider{}, nil
+	})
 }
